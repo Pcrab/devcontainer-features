2b102a60790f91967acaf49819edf114b03073f9
pcrab <pcrab@pcrab.xyz>
2024-04-05 19:08:12 +0800

    fix: grep error

--- test/lsd/lsd-alpine.sh
+++ test/lsd/lsd-alpine.sh
@@ -7,6 +7,6 @@ source dev-container-features-test-lib
 
 echo "lsd version"
 LSD_VERSION=$(lsd --version)
-check "check ls version" bash -c "ls --version | grep ${LSD_VERSION}"
+check "ls version" bash -c "ls --version | grep '${LSD_VERSION}'"
 
 reportResults
